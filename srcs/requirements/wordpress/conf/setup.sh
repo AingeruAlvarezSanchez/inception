db5e07a6fe00a33c8bd2cea964fbf7dc116e3f5c
aalvarez <aalvarez@aalvarez.aalvarez.42.fr>
2023-03-23 09:55:21 +0000

    theme fixed

--- srcs/requirements/wordpress/conf/setup.sh
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -2,11 +2,6 @@
 
 mkdir -p /var/www/html/
 
-while ! nc -z mariadb 3306; do
-  echo "Esperando a que el contenedor 'db' esté disponible..."
-  sleep 1
-done
-
 cd /var/www/html
 
 wp core download --locale=es_ES --allow-root
@@ -15,5 +10,5 @@ wp --path=/var/www/html config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQ
 
 wp core install --path=/var/www/html  --url=${DOMAIN_NAME} --title="sabias que un 90% de los koalas tienen clamidia?" --admin_name=${WP_ADMIN_USER} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email="aalvarez@student.42urduliz.com" --skip-email --allow-root
 
-wp --path=/var/www/html theme install https://downloads.wordpress.org/theme/scapeshot.1.2.zip --allow-root
-wp --path=/var/www/html theme activate scapeshot --allow-root
+wp --path=/var/www/html theme install https://downloads.wordpress.org/theme/photobrust.1.0.1.zip --allow-root
+wp --path=/var/www/html theme activate photobrust --allow-root
